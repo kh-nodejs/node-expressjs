b328e8007339a64a54369af019ced34af2ee8f10
khrist14n@gmail.com <khrist14n@gmail.com>
2024-08-23 21:21:20 -0500

    server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const { createClient } = require("@supabase/supabase-js");
 
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 3000;
 
