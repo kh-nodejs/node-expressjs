4be70a0e8437e3e073b229b9327713aeb2968ed2
khrist14n@gmail.com <khrist14n@gmail.com>
2024-08-23 21:05:42 -0500

    server

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
 
-app.get('/', (req, res) => {
+const { createClient } = require("@supabase/supabase-js");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
   res.json({
-    message: 'Hola Mundo',
-  })
+    message: "Hola Mundo",
+  });
 });
 
-app.get('/one', (req, res) => {
+app.get("/one", (req, res) => {
   res.json({
-    message: 'One',
-  })
+    message: "One",
+  });
 });
 
-app.get('/two', (req, res) => {
+app.get("/two", (req, res) => {
   res.json({
-    message: 'Two',
-  })
+    message: "Two",
+  });
 });
 
-app.get('/three', (req, res) => {
+app.get("/three", (req, res) => {
   res.json({
-    message: 'Three',
-  })
+    message: "Three",
+  });
 });
 
-app.get('/four', (req, res) => {
+app.get("/four", (req, res) => {
   res.json({
-    message: 'Four',
-  })
+    message: "Four",
+  });
+});
+
+app.get("/five", (req, res) => {
+  const supabaseUrl = "https://obpglrrzjmajyxslbowq.supabase.co";
+  const supabaseKey = process.env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  res.json({
+    message: "Five",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`)
-})
+  console.log(`App is listening on port ${port}`);
+});
